e8885eeb48b024878108e687e90597f15bbf4dfe
Mike DuVall <mike.a.duvall@gmail.com>
2016-10-28 07:10:49 -0500

    Fixed texture/sprite size

--- Mushroom.h
+++ Mushroom.h
@@ -13,8 +13,12 @@ namespace mushroomNS
 {
 	//const int   WIDTH = 128;                // image width
 	//const int   HEIGHT = 128;               // image height
-	const int   WIDTH = 50;                // image width
-	const int   HEIGHT = 39;               // image height
+	//const int   WIDTH = 50;                // image width
+	//const int   HEIGHT = 39;               // image height
+
+	const int   WIDTH = 64;                // image width
+	const int   HEIGHT = 64;               // image height
+
 
 	const int   X = GAME_WIDTH / 2 - WIDTH / 2; // location on screen
 	const int   Y = GAME_HEIGHT / 2 - HEIGHT / 2;
