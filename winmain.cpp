44ce1b0223191b56f32548c4afa84974cdd501b7
mduvall <mduvall@WIN7-BARNEY>
2017-01-25 08:07:12 -0600

    Pulled handling of test mode rest call into separate class

--- winmain.cpp
+++ winmain.cpp
@@ -11,140 +11,11 @@
 #include <chrono>
 #include <thread>
 #include "Minigunner.h"
-
-
-#include <cpprest\http_listener.h>
-
-
-using namespace web::http::experimental::listener;
-using namespace web::http;
-using namespace web;
-
-
-http_listener * gdiMinigunnerListener;
-http_listener * nodMinigunnerListener;
-http_listener * leftClickListener;
+#include "TestModeRestHandler.h"
 
 
 Game * game = NULL;
 
-
-
-void ClickLeftMouseButton(int x, int y) {
-
-	double fScreenWidth = ::GetSystemMetrics(SM_CXSCREEN) - 1;
-	double fScreenHeight = ::GetSystemMetrics(SM_CYSCREEN) - 1;
-	double fx = x*(65535.0f / fScreenWidth);
-	double fy = y*(65535.0f / fScreenHeight);
-
-	INPUT mouseInput = { 0 };
-	mouseInput.type = INPUT_MOUSE;
-	mouseInput.mi.dx = (LONG)fx;
-	mouseInput.mi.dy = (LONG)fy;
-	mouseInput.mi.mouseData = 0;
-
-
-	mouseInput.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
-	::SendInput(1, &mouseInput, sizeof(INPUT));
-
-	mouseInput.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;;
-	::SendInput(1, &mouseInput, sizeof(INPUT));
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	mouseInput.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-	::SendInput(1, &mouseInput, sizeof(INPUT));
-}
-
-
-std::pair<int, int>  ParseMinigunnerRequest(http_request message) {
-	pplx::task<json::value> jsonValue = message.extract_json();
-	web::json::value webJsonValue = jsonValue.get();
-	web::json::object object = webJsonValue.as_object();
-
-	int minigunnerX = -666;
-	int minigunnerY = -666;
-
-	for (auto iter = object.cbegin(); iter != object.cend(); ++iter) {
-		utility::string_t attributeName = iter->first;
-		//const json::value &str = iter->first;
-		const json::value &v = iter->second;
-
-		if (attributeName == L"x") {
-			minigunnerX = v.as_integer();
-		}
-
-		if (attributeName == L"y") {
-			minigunnerY = v.as_integer();
-		}
-	}
-
-	std::pair<int, int> xAndY = std::make_pair(minigunnerX, minigunnerY);
-	return xAndY;
-}
-
-void HandlePostGdiMinigunner(http_request message) {
-	std::pair<int, int> xAndY = ParseMinigunnerRequest(message);
-	game->AddCreateGDIMinigunnerEvent(xAndY.first, xAndY.second);
-	// TODO:  update this to return the created minigunner as JSON, instead of result message
-    message.reply(status_codes::OK, U("Initialized minigunner"));
-};
-
-
-void HandlePostNodMinigunner(http_request message) {
-	std::pair<int, int> xAndY = ParseMinigunnerRequest(message);
-	game->AddCreateNODMinigunnerEvent(xAndY.first, xAndY.second);
-	// TODO:  update this to return the created minigunner as JSON, instead of result message
-	message.reply(status_codes::OK, U("Initialized minigunner"));
-};
-
-
-void RenderAndReturnMinigunner(http_request message, Minigunner * minigunner) {
-	json::value obj;
-	obj[L"x"] = json::value::number(minigunner->getX());
-	obj[L"y"] = json::value::number(minigunner->getY());
-	obj[L"health"] = json::value::number(minigunner->GetHealth());
-	message.reply(status_codes::OK, obj);
-}
-
-
-
-void HandleGetGdiMinigunner(http_request message) {
-	Minigunner * minigunner = game->GetGDIMinigunnerViaEvent();
-	RenderAndReturnMinigunner(message, minigunner);
-};
-
-
-void HandleGetNodMinigunner(http_request message) {
-	Minigunner * minigunner = game->GetNODMinigunnerViaEvent();
-	RenderAndReturnMinigunner(message, minigunner);
-};
-
-
-void handlePOSTLeftClick(http_request message) {
-	pplx::task<json::value> jsonValue = message.extract_json();
-	web::json::value webJsonValue = jsonValue.get();
-	web::json::object object = webJsonValue.as_object();
-
-	int mouseX = -666;
-	int mouseY = -666;
-
-	for (auto iter = object.cbegin(); iter != object.cend(); ++iter) {
-		utility::string_t attributeName = iter->first;
-		//const json::value &str = iter->first;
-		const json::value &v = iter->second;
-
-		if (attributeName == L"x") {
-			mouseX = v.as_integer();
-		}
-
-		if (attributeName == L"y") {
-			mouseY = v.as_integer();
-		}
-	}
-
-	ClickLeftMouseButton(mouseX, mouseY);
-	message.reply(status_codes::OK, U("Success"));
-};
 
 
 //Next:
@@ -168,7 +39,6 @@ void init_input(HWND hWnd);
 
 HWND hwnd = NULL;
 
-std::wstring baseUrl = L"http://*:11369";
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     LPSTR lpCmdLine, int nCmdShow)
@@ -197,23 +67,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
         game->initialize(hwnd);     // throws GameError
 		init_input(hwnd);
 
-		std::wstring gdiMinigunnerURL = baseUrl + L"/mac/gdiMinigunner";
-		gdiMinigunnerListener = new http_listener(gdiMinigunnerURL);
-		gdiMinigunnerListener->open().wait();
-		gdiMinigunnerListener->support(methods::POST, HandlePostGdiMinigunner);
-		gdiMinigunnerListener->support(methods::GET, HandleGetGdiMinigunner);
-
-
-		std::wstring nodMinigunnerURL = baseUrl + L"/mac/nodMinigunner";
-		nodMinigunnerListener = new http_listener(nodMinigunnerURL);
-		nodMinigunnerListener->open().wait();
-		nodMinigunnerListener->support(methods::POST, HandlePostNodMinigunner);
-		nodMinigunnerListener->support(methods::GET, HandleGetNodMinigunner);
-
-		std::wstring leftClickURL = baseUrl + L"/mac/leftClick";
-		leftClickListener = new http_listener(leftClickURL);
-		leftClickListener->open().wait();
-		leftClickListener->support(methods::POST, handlePOSTLeftClick);
+		TestModeRestHandler * testModeRestHandler = new TestModeRestHandler(game);
 
         int done = 0;
         while (!done)
