738032a2eef3192d51f26db72a29e1b82d10ce4e
Mike DuVall <mike.a.duvall@gmail.com>
2017-04-18 07:09:12 -0500

    Fixed previous commit

--- Minigunner.cpp
+++ Minigunner.cpp
@@ -30,7 +30,8 @@ Minigunner::Minigunner(Game * game, Graphics * graphics, int x, int y, UnitSelec
 	velocity.x = 150.0;
 	velocity.y = 150.0;
 
-	gameSprite = new GameSprite(graphics->Get3Ddevice(), IMAGE_FILE, this->width, this->height, graphicsNS::WHITE);
+	//gameSprite = new GameSprite(graphics->Get3Ddevice(), IMAGE_FILE, this->width, this->height, graphicsNS::WHITE);
+	gameSprite = new GameSprite(graphics->Get3Ddevice(), "", this->width, this->height, graphicsNS::WHITE);
 }
 
 
